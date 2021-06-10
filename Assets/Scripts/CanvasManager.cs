8a215a13ee068d48cd4fdc8f5165d30726712410
caishen233 <caishen@89trillion.com>
2021-06-10 17:56:07 +0800

    解决了购买三次后不能再购买和再次购买没有播放开箱动画Bug，分工了脚本职能，代码更简洁易懂

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    //开始按钮
+    [SerializeField] private Button openButton;
+    //宝箱界面
+    [SerializeField] private Image boxImage;
+    /// <summary>
+    /// openButton的点击事件，激活宝箱界面
+    /// </summary>
+    public void OpenViewOnCLick()
+    {
+        openButton.gameObject.SetActive(false);
+        boxImage.gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// 初始化激活状态
+    /// </summary>
+    void Start()
+    {
+        boxImage.gameObject.SetActive(false);
+    }
+}
